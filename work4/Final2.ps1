40f7a81916fa45e24de94c86785b755f77e9e18d
Noralik <158551209+Noralik@users.noreply.github.com>
2025-04-26 22:58:03 +0300

    Delete work4/Final2.ps1

--- work4/Final2.ps1
+++
@@ -1,4 +0,0 @@
-cd C:\Users\$env:UserName\Desktop\$env:UserName
-cp * C:\new_drive\all_users\$env:UserName
-cd C:\Users\$env:UserName\Desktop\$env:UserName
-Remove-Item C:\Users\$env:UserName\Desktop\$env:Username\* -Recurse
